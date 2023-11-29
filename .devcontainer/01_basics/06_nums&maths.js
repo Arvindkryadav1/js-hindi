e7d173db418027f6455891cec08783984e6f4145
Arvindkryadav1 <140964372+Arvindkryadav1@users.noreply.github.com>
2023-11-29 09:05:23 +0000

    Math and num

---
+++ .devcontainer/01_basics/06_nums&maths.js
@@ -0,0 +1,29 @@
+const score = 400
+// console.log(score)
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(score.toString())
+// console.log(score.toFixed(1))
+
+const otherNumber = 34.654867
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//++++++++++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.floor(67.24));
+// console.log(Math.round(67.64));
+// console.log(Math.min(4, 6, 7, ));
+// console.log(Math.max(4, 6, 7,));
+
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max-min + 1) + min))
